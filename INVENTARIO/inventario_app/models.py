d403e2cd4f68c95353f63c477e9fc1cb4f4d3fa1
Paola Falcon <cacoepy@gmail.com>
2013-08-04 20:26:43 -0600

    Modelo
    Categoría y Producto

--- INVENTARIO/inventario_app/models.py
+++ INVENTARIO/inventario_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class  Categoria(models.Model):
+	idcategoria = models.IntegerField
+	descripcion = models.CharField(max_length=200)
+
+class Producto(models.Model):
+	idproducto = models.IntegerField
+	idcategoria = models.ForeignKey(Categoria)
+	descripcion = models.CharField(max_length=200)
+	stock = models.IntegerField
